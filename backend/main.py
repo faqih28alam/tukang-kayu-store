8f8130523b1ebcb28ee29173c819b2d7e7d50871
faqih28alam <faqih28alam@gmail.com>
2026-04-20 12:28:42 +0700

    feat: add endpoint /name

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Define a Simple Route
 @app.get("/")
 def root():
     return {"message": "Hello World!"}
+
+#
+@app.get("/name")
+def root(name: str="Budi"):
+    return{"message": f"Selamat datang di toko kami, kak {name}!"}
